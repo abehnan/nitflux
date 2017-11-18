33248f302b3527fdfa170a553371ef17849dab4c
evan <evan.s.roach@gmail.com>
2017-11-18 15:56:14 -0500

    Here we have a Python script to get useful information from each film's unique page. It takes jsonEntries.txt and outputs allInfo.json. Newline between each film's json representation.
    Issue: some characters are in unicode or some non-character format.

---
+++ moreInfo.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import urllib
+import json
+x=1
+with open("jsonEntries.txt", 'r') as source:
+    with open('allInfo.json', 'w') as dest:
+
+    # one film at a time
+        for line in source:
+            entry = json.loads(line)
+            page = entry['page']
+            siteHtml = urllib.urlopen(page)
+            soup = BeautifulSoup(siteHtml, "html.parser")
+
+            year = soup.find("span", { "class" : "year" })
+            if (year != None):
+                year = year.text
+            rating = soup.find("span", { "class" : "maturity-number" })
+            if (rating != None):
+                rating = rating.text
+            duration = soup.find("span", { "class" : "test_dur_str" })
+            if (duration != None):
+                duration = duration.text
+            synopsis = soup.find("p", { "class" : "synopsis" })
+            if (synopsis != None):
+                synopsis = synopsis.text
+            actors = soup.find("span", { "class" : "actors-list" })
+            if (actors != None):
+                actors = actors.text
+            genres = soup.find("span", { "class" : "genre-list" })
+            if (genres != None):
+                genres = genres.text
+            print x
+            x += 1
+            dct = {"year":year, "rating":rating, "duration":duration,
+            "synopsis":synopsis, "actors":actors, "genres":genres}
+            entry.update(dct)
+            json.dump(entry, dest)
+            dest.write('\n')
