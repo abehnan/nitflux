2892538dfae8000c39fee8e0578aea0965e9714a
abehnan <abehnan@gmail.com>
2017-12-12 21:41:27 -0500

    form style improvement on entry.php

--- entry.php
+++ entry.php
@@ -118,14 +118,14 @@
       echo "</br><strong>Enter your review below:</strong></br></br>";
       echo "<form name=\"comment\" action=\"http://localhost/nitflux/addComment\"
          method=\"get\" style=\"font-weight: bold;\">";
-      echo "<input type=\"hidden\" name=\"movie\" value=\"$movie\">";
+      echo "<input class=\"input\" type=\"hidden\" name=\"movie\" value=\"$movie\">";
       echo "Name: &nbsp;";
-      echo "<input type=\"text\" name=\"reviewer\" maxlength=\"16\" required>";
+      echo "<input class=\"input\" type=\"text\" name=\"reviewer\" maxlength=\"16\" required>";
       echo "</br>";
       echo "Rating: &nbsp;";
-      echo "<input type=\"number\" name=\"rating\" max=\"10\" min=\"1\" required>";
+      echo "<input class=\"input\" type=\"number\" name=\"rating\" max=\"10\" min=\"1\" required>";
       echo "</br>";
-      echo "<input type=\"text\" name=\"data\" style=\"width: 90%; height: 200px;\"
+      echo "<input class=\"input\" type=\"text\" name=\"data\" style=\"width: 90%; height: 200px;\"
        maxlength=\"500\" required>";
       echo "</br>";
       echo "<input type=\"submit\" class=\"mybutton\" value=\"Submit\">";
